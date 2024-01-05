eeb511257e8509c90de981545e80a541fd1ce525
C-odingNomad <zeeblak@yahoo.com>
2024-01-05 21:39:35 +0500

    started creating calculator

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-export {};
+import inquirer from "inquirer";
+const calculator = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Enter 1st Numeric Value:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Enter 2nd Numeric Value:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select operation to perform: ",
+        choices: ["Addition +", "Subtraction -", "Multiplication x", "Division /"]
+    }
+]);
--- app.ts
+++ app.ts
@@ -1,2 +1,24 @@
 import inquirer from "inquirer";
 
+const calculator:{
+    numberOne: number,
+    numberTwo: number,
+    operator: string
+} = await inquirer.prompt([
+    {
+        type:"number",
+        name:"numberOne",
+        message: "Enter 1st Numeric Value:"
+    },
+    {
+        type:"number",
+        name:"numberTwo",
+        message: "Enter 2nd Numeric Value:"
+    },
+    {
+        type:"list",
+        name:"operator",
+        message: "Select operation to perform: ",
+        choices: ["Addition +", "Subtraction -", "Multiplication x", "Division /"]
+    }
+])
